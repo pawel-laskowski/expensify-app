6c872fd0745cb236ae0474c2150fbde35afba95c
Pawel Laskowski <pawel-laskowski@outlook.com>
2021-11-23 19:11:12 +0100

    Setup production build and server

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import Header from '../components/Header'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
-import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
 
 const AppRouter = () => (
@@ -14,7 +13,6 @@ const AppRouter = () => (
       <Route path="/" element={<ExpenseDashboardPage />} />
       <Route path="/create" element={<AddExpensePage />} />
       <Route path="/edit/:id" element={<EditExpensePage />} />
-      <Route path="/help" element={<HelpPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
